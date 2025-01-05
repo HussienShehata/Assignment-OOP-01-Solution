04fbe358e8f63bf9c0a77a4c3a20aea9b1a42f39
LAPTOP-5AR3TGQ9\Hussien <hussienehab616@gmail.com>
2025-01-05 22:40:04 +0200

    Add project files.

---
+++ Assignment01/Program.cs
@@ -0,0 +1,48 @@
+﻿using AssignmentClassLibarary01;
+namespace Assignment01
+
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1
+
+            //WeekDays? day;
+            //bool isParsed;
+            //do
+            //{
+               
+            //    isParsed = Enum.TryParse<WeekDays>(Console.ReadLine(), out day);
+            //    Console.WriteLine(day);
+            //} while (!isParsed || day is  null); 
+
+            //for(int i=0; i<7;i++)
+            //{
+            //    day = (WeekDays)i;
+            //    Console.WriteLine(day);
+            //}
+            #endregion
+
+
+            #region Q2
+
+            Seas_on? season;
+            bool isParsed;
+
+            do
+            {
+                isParsed = Enum.TryParse<Seas_on>(Console.ReadLine(), out );
+            } while (!isParsed || season is null);
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
---
+++ AssignmentClassLibarary01/Seas on.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentClassLibarary01
+{
+    public enum Seas_on
+    {
+        none,
+        march=1 ,
+        april=1,
+        may=1,
+        spring=1,
+
+        june=2,
+        july=2,
+        august=2,
+        summer=2,
+
+        september=3,
+        october=3,
+        novemebr=3,
+        autumn=3,
+
+        decemeber=4,
+        january=4,
+        february=4,
+        winter=4
+    }
+}
